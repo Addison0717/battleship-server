d9e2c172776bd9c4e0295361da73d06195555a55
Addison Schultz <addisonschultz0@gmail.com>
2018-07-13 10:47:26 +0200

    deploying server to heroku

---
+++ target/db.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeorm_1 = require("typeorm");
+const DefaultNamingStrategy_1 = require("typeorm/naming-strategy/DefaultNamingStrategy");
+const StringUtils_1 = require("typeorm/util/StringUtils");
+const entity_1 = require("./users/entity");
+const entities_1 = require("./games/entities");
+class CustomNamingStrategy extends DefaultNamingStrategy_1.DefaultNamingStrategy {
+    tableName(targetName, userSpecifiedName) {
+        return userSpecifiedName ? userSpecifiedName : StringUtils_1.snakeCase(targetName) + 's';
+    }
+    columnName(propertyName, customName, embeddedPrefixes) {
+        return StringUtils_1.snakeCase(embeddedPrefixes.concat(customName ? customName : propertyName).join("_"));
+    }
+    columnNameCustomized(customName) {
+        return customName;
+    }
+    relationName(propertyName) {
+        return StringUtils_1.snakeCase(propertyName);
+    }
+}
+exports.default = () => typeorm_1.createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL || 'postgres://flnkqflbxsjqiw:b6fa44f5050ab0b4c67d71416fc24e4f7691a7f9f09e46fc3a3bbe46a7d6cff0@ec2-54-217-235-159.eu-west-1.compute.amazonaws.com:5432/df797j54u9ge70',
+    entities: [
+        entity_1.default,
+        entities_1.Player,
+        entities_1.Game
+    ],
+    synchronize: true,
+    logging: true,
+    namingStrategy: new CustomNamingStrategy()
+})
+    .then(_ => console.log('Connected to Postgres with TypeORM'));
+//# sourceMappingURL=db.js.map
